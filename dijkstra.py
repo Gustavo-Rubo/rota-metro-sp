85e9e46835b563c0a9674e432edb88770e59c553
gustavorubo <gustavo.rubo@hotmail.com>
2024-06-09 14:37:16 -0300

    dijkstra solver

---
+++ dijkstra.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+grafo = [
+    [0, 1, 9, 9],
+    [1, 0, 1, 9],
+    [9, 1, 0, 3],
+    [9, 9, 3, 0]
+]
+
+def dijkstra(graph):
+    for start in range(len(graph)-1):
+        unvisited = [i for i in range(len(graph)) if i != start]
+
+        while (len(unvisited) > 1):
+            next = -1
+            min_distance = np.inf
+            for u in unvisited:
+                distance = graph[start][u]
+                if (distance <= min_distance):
+                    min_distance = distance
+                    next = u
+
+            unvisited.remove(next)
+            for u in unvisited:
+                distance1 = graph[start][u]
+                distance2 = graph[start][next] + graph[next][u]
+                if (distance2 < distance1):
+                    graph[start][u] = distance2
+                    graph[u][start] = distance2
+
+    # print('\n'.join([','.join([f'{n:.1f}' for n in row]) for row in grafo]))
