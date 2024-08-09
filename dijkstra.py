f7c8af81b022d1be29838c81e3a385b245458513
rubo <gustavo.rubo@hotmail.com>
2024-08-09 17:01:57 -0300

    add paths to dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -7,28 +7,38 @@ grafo = [
     [9, 9, 3, 0]
 ]
 
+
 def dijkstra(graph_simple):
-    graph = graph_simple.copy()
-    for start in range(len(graph)-1):
-        unvisited = [i for i in range(len(graph)) if i != start]
+    distances = graph_simple.copy()
+    paths = []
+    for i in range(len(distances)):
+        paths.append([])
+        for j in range(len(distances[0])):
+            path = [] if distances[i][j] == np.inf else [j]
+            paths[i].append(path)
+
+    for start in range(len(distances)-1):
+        unvisited = [i for i in range(len(distances)) if i != start]
 
         while (len(unvisited) > 1):
             next = -1
             min_distance = np.inf
             for u in unvisited:
-                distance = graph[start][u]
+                distance = distances[start][u]
                 if (distance <= min_distance):
                     min_distance = distance
                     next = u
 
-            unvisited.remove(next)
             for u in unvisited:
-                distance1 = graph[start][u]
-                distance2 = graph[start][next] + graph[next][u]
-                if (distance2 < distance1):
-                    graph[start][u] = distance2
-                    graph[u][start] = distance2
-    
-    return graph
+                distance1 = distances[start][u]
+                distance2 = distances[start][next] + distances[next][u]
+                if (distance2 <= distance1):
+                    distances[start][u] = distance2
+                    distances[u][start] = distance2
+                    paths[start][u] = paths[start][next]+[next]+paths[next][u]+[u]
+                    paths[u][start] = paths[u][next]+[next]+paths[next][start]+[start]
 
-    # print('\n'.join([','.join([f'{n:.1f}' for n in row]) for row in grafo]))
+            unvisited.remove(next)
+
+    return distances, paths
+    # print('\n'.join([','.join([f'{n:.1f}' for n in row]) for row in distances]))
